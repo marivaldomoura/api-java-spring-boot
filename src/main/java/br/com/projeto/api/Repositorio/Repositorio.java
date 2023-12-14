5dd4343bae83156c5a8bf4be1f49e705185d1abd
moura <mourasantos12@gmail.com>
2023-12-14 19:42:52 -0300

    api java spring boot primeira versao

---
+++ src/main/java/br/com/projeto/api/Repositorio/Repositorio.java
@@ -0,0 +1,12 @@
+package br.com.projeto.api.Repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.projeto.api.modelo.Cliente;
+
+@Repository
+public interface Repositorio extends CrudRepository<Cliente,Long>{
+
+    
+}
